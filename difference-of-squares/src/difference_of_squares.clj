14641a3b3544247548985c89f4d455b4317d9d06
nicolasalmini <nicolas.salmini@gmail.com>
2025-05-22 12:44:53 -0300

    Difference of squares exercise

--- difference-of-squares/src/difference_of_squares.clj
+++ difference-of-squares/src/difference_of_squares.clj
@@ -1,19 +1,26 @@
 (ns difference-of-squares)
 
+(defn- square [x]
+  (* x x))
+
 (defn square-of-sum
   "Returns the square of the sum of the numbers up to the given number"
   [n]
-  ;; function body
-  )
+  (->>
+    (range 1 (inc n))
+    (reduce +)
+    (square)))
 
 (defn sum-of-squares
   "Returns the sum of the squares of the numbers up to the given number"
   [n]
-  ;; function body
-  )
+  (->>
+    (range 1 (inc n))
+    (map square)
+    (reduce +)))
 
 (defn difference
   "Returns the difference between the square of the sum of numbers up to a given number and the sum of the squares of those numbers"
   [n]
-  ;; function body
+  (- (square-of-sum n) (sum-of-squares n))
   )
