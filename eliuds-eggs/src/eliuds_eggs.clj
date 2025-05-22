f20bb9d6b312799414e0e737b40cb1a44a9afee9
nicolasalmini <nicolas.salmini@gmail.com>
2025-05-22 15:53:21 -0300

    Add solution to exercise

--- eliuds-eggs/src/eliuds_eggs.clj
+++ eliuds-eggs/src/eliuds_eggs.clj
@@ -3,5 +3,6 @@
 (defn egg-count
   "Returns the number of 1 bits in the binary representation of the given number."
   [number]
-  ;; function body
-  )
+  (if (zero? number)
+    0
+    (+ (mod number 2) (egg-count (quot number 2)))))
