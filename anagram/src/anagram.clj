9ae41332cb1abe31119fbec770e82f63051f1406
nicolasalmini <nicolas.salmini@gmail.com>
2025-07-25 22:28:15 -0300

    oopsie in anagram exercise

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,17 +1,13 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(println "anagram.clj file is being loaded!")
-
 (defn anagram? [s1 s2]
   (let [lcs1 (str/lower-case s1)
         lcs2 (str/lower-case s2)]
     (and (not= lcs1 lcs2)
          (= (sort lcs1) (sort lcs2)))))
 
-
 (defn anagrams-for
   [target-word
    candidates]
-  ;; (filter (partial anagram? target-word) candidates))
   (filter #(anagram? target-word %) candidates))
